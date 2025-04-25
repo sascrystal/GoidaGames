bdaeadaf2881e9f124fc49d603939e8929daa723
sascrystal <ru.da.sas@bk.ru>
2025-04-25 20:36:14 +0300

    SkeletonHalberd фиксы

--- core/src/main/java/io/github/some_example_name/enemy_classes/enemies/SkeletonHalberd.java
+++ core/src/main/java/io/github/some_example_name/enemy_classes/enemies/SkeletonHalberd.java
@@ -9,9 +9,7 @@ import com.badlogic.gdx.math.Rectangle;
 import io.github.some_example_name.buffs.modifier_buffs.Weakness;
 import io.github.some_example_name.enemy_classes.enemy_moves.AttackEnemy;
 import io.github.some_example_name.enemy_classes.enemy_moves.DebuffAttackEnemy;
-import io.github.some_example_name.enemy_classes.enemy_moves.DebuffMove;
 import io.github.some_example_name.enemy_classes.enemy_moves.MoveEnemy;
-import io.github.some_example_name.enemy_classes.enemy_moves.SelfHarm;
 import io.github.some_example_name.screens.GameScreen;
 
 public class SkeletonHalberd extends  Enemy{
@@ -29,7 +27,7 @@ public class SkeletonHalberd extends  Enemy{
             (float) (texture.getHeight())*7);
 
         health = 30; // Установка здоровья
-        moveList = new MoveEnemy[3];// Установка массива возможностей моба
+        moveList = new MoveEnemy[3];// Установка массива возможностей enemy
         moveList[0] = new AttackEnemy(6);
         moveList[1] = new AttackEnemy(8);
         moveList[2] = new DebuffAttackEnemy(3,new Weakness());
