8fa3a5b6e1a3472e1b9ce68182e732e00260726f
sascrystal <ru.da.sas@bk.ru>
2025-04-25 20:51:25 +0300

    добавлены карты

--- core/src/main/java/io/github/some_example_name/cards/target_cards/Overload.java
+++ core/src/main/java/io/github/some_example_name/cards/target_cards/Overload.java
@@ -1,5 +1,8 @@
 package io.github.some_example_name.cards.target_cards;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 import io.github.some_example_name.buffs.modifier_buffs.OverloadBuff;
 import io.github.some_example_name.enemy_classes.enemies.Enemy;
 
@@ -10,6 +13,7 @@ public class Overload extends TargetCard {
     public Overload() {
         name = "Перегрузка";
         description = "Накладывает дебафф <<Перегрузка>>";
+        texture = new Texture(Gdx.files.internal("cards/cardReload.png"));
         cost = 3;
     }
 
--- core/src/main/java/io/github/some_example_name/cards/target_cards/attack_cards/PhantomPain.java
+++ core/src/main/java/io/github/some_example_name/cards/target_cards/attack_cards/PhantomPain.java
@@ -1,5 +1,8 @@
 package io.github.some_example_name.cards.target_cards.attack_cards;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 import io.github.some_example_name.buffs.MentalDamage;
 import io.github.some_example_name.enemy_classes.enemies.Enemy;
 import io.github.some_example_name.player.Player;
@@ -8,6 +11,7 @@ public class PhantomPain extends CardAttack {
     public PhantomPain() {
         name = "Фантомная боль";
         description = "тип: атака. Наносит 8 урона. Накладывает дебафф ментальные повреждения";
+        texture = new Texture(Gdx.files.internal("cards/cardPhantomPain.png"));
         damage = 8;
         cost = 1;
     }
